f83b824b4d88feaa7fef4654b119735bd89ea7dd
shivambh1717 <shivambhargav1717@gmail.com>
2025-07-22 11:31:42 -0400

    removed issues

---
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,41 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import clientPromise from "@/lib/mongodb";
+import { compare } from "bcryptjs";
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const client = await clientPromise;
+        const db = client.db('Divyanshu_database');
+
+        const user = await db.collection("users").findOne({ email: credentials?.email });
+
+        if (user && await compare(credentials!.password, user.password)) {
+          return {
+            id: user._id.toString(),
+            name: user.name,
+            email: user.email,
+          };
+        }
+
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export { authOptions }; 
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,42 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import clientPromise from "@/lib/mongodb";
-import { compare } from "bcryptjs";
-
-const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        const client = await clientPromise;
-        const db = client.db('Divyanshu_database');
-
-        const user = await db.collection("users").findOne({ email: credentials?.email });
-
-        if (user && await compare(credentials!.password, user.password)) {
-          return {
-            id: user._id.toString(),
-            name: user.name,
-            email: user.email,
-          };
-        }
-
-        return null;
-      },
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-  pages: {
-    signIn: "/login",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import NextAuth from "next-auth";
+import { authOptions } from "./authOptions";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
